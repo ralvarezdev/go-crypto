42785a971fbe226bef4ed9e4f337e4e70bdb43b1
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-12-18 21:48:21 -0400

    refactor: Moved JWT implementations to 'github.com/ralvarezdev/go-jwt'

--- jwt/errors.go
+++
@@ -1,9 +0,0 @@
-package jwt
-
-import "errors"
-
-var (
-	UnableToParsePrivateKeyError = errors.New("unable to parse private key")
-	UnableToParsePublicKeyError  = errors.New("unable to parse public key")
-	InvalidKeyTypeError          = errors.New("invalid key type")
-)
