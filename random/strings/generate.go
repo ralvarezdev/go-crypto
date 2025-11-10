72dd93ab22952c24b08ecf10cc2ce9b9589771b6
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-11-10 16:24:03 -0400

    refactor: modified structure of the random strings package to avoid confussions

--- random/strings/generate.go
+++ random/strings/generate.go
@@ -1,7 +1,11 @@
 package strings
 
 import (
+	"crypto/rand"
 	"encoding/hex"
+	"math/big"
+
+	gostrings "github.com/ralvarezdev/go-strings"
 
 	gocryptorandombytes "github.com/ralvarezdev/go-crypto/random/bytes"
 )
@@ -37,7 +41,7 @@ func Generate(length int) (string, error) {
 //   - An error if there was an issue generating any of the random strings.
 func GenerateN(n, length int) ([]string, error) {
 	strings := make([]string, n)
-	for i := 0; i < n; i++ {
+	for i := range strings {
 		randomString, err := Generate(length)
 		if err != nil {
 			return nil, err
@@ -45,4 +49,67 @@ func GenerateN(n, length int) ([]string, error) {
 		strings[i] = randomString
 	}
 	return strings, nil
+}
+
+// GenerateAlphanumeric generates a random alphanumeric string of the specified length
+//
+// Parameters:
+//
+//   - length: The length of the random string to generate
+//
+// Returns:
+//
+//   - string: The generated random string
+//   - error: An error if something went wrong
+func GenerateAlphanumeric(length int) (string, error) {
+	result := make([]byte, length)
+	for i := range result {
+		randomIndex, err := rand.Int(
+			rand.Reader,
+			big.NewInt(int64(len(gostrings.AlphanumericCharset))),
+		)
+		if err != nil {
+			return "length", err
+		}
+		result[i] = gostrings.AlphanumericCharset[randomIndex.Int64()]
+	}
+	return string(result), nil
+}
+
+// GenerateNAlphanumeric generates N random alphanumeric strings of the specified length
+//
+// Parameters:
+//
+//   - n: The number of random strings to generate
+//   - length: The length of each random string to generate
+//
+// Returns:
+//
+//   - []string: A slice of generated random strings
+//   - error: An error if something went wrong
+func GenerateNAlphanumeric(n, length int) ([]string, error) {
+	strings := make([]string, n)
+	for i := range strings {
+		randomString, err := Generate(length)
+		if err != nil {
+			return nil, err
+		}
+		strings[i] = randomString
+	}
+	return strings, nil
+}
+
+// GenerateRecoveryCodes generates recovery codes with a count and a length
+//
+// Parameters:
+//
+//   - count: The number of recovery codes to generate
+//   - length: The length of each recovery code to generate
+//
+// Returns:
+//
+//   - []string: A slice of generated recovery codes
+//   - error: An error if something went wrong
+func GenerateRecoveryCodes(count, length int) ([]string, error) {
+	return GenerateN(count, length)
 }
